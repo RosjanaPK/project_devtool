ed512ff3ef6cda4f9ef04e3d37372a621e40837f
RosjanaPK <rajna471@gmail.com>
2023-04-14 16:24:07 +0700

    loop html

--- Lab_jenkins_dockercompose/web/frontend/app.py
+++ Lab_jenkins_dockercompose/web/frontend/app.py
@@ -1,5 +1,5 @@
 import os
-from flask import Flask, render_template, send_file
+from flask import Flask, render_template, send_file, request
 import cv2
 import requests
 import base64
@@ -9,34 +9,41 @@ import matplotlib.pyplot as plt
 import numpy as np
 import cv2
 import json
+from PyPDF2 import PdfReader
 
 
 app = Flask(__name__)
 
 url = "http://localhost:8000"
 
-@app.route('/')
-def index():
-    pdf_file_path = 'Coop101.pdf'
+@app.route('/document.html')
+def document():
+    My_list = []
+    folder_path = "./document"
+    for file in os.listdir(folder_path):
+        pdf_path = os.path.join(folder_path, file)
+        payload = {
+            "file_name": file,
+            "file_path": pdf_path,
+        }
+        response = requests.post(f"{url}/process-file", json=payload)
 
-    pdf_file_name = os.path.basename(pdf_file_path)
-    pdf_file_full_path = os.path.abspath(pdf_file_path)
-    payload = {
-        "file_name": pdf_file_name,
-        "file_path": pdf_file_full_path,
-    }
-    response = requests.post(f"{url}/process-file", json=payload)
+        data = json.loads(response.content)
+        processed_file_string = data["file_name"]
+        processed_file_path_string = data["file_path"]
+        My_list.append([processed_file_string,  processed_file_path_string])
 
-    data = json.loads(response.content)
-
-    processed_image_string = data["file_name"]
-    return render_template("document.html", mycontent= processed_image_string)
+    return render_template("document.html", my_array=My_list)
+              
 
 @app.route('/download')
 def download_file():
-    pdf_file_path = 'Coop101.pdf'
-    path= os.path.abspath(pdf_file_path)
-    return send_file(path, as_attachment=True)
+    filename = request.args.get('filename')
+    return send_file(filename, as_attachment=True)
+
+@app.route('/index.html')
+def index():
+    return render_template("index.html")
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0",port="8081")
